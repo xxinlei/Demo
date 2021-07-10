806c4428a4358bf8e066e5d5b0140a733fbd40d5
xinlei <xxinlei13@gmail.com>
2021-07-10 09:30:16 +0800

    none

--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sadakoImage: UIImageView!
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBOutlet weak var mySwitch: UISwitch!
+    
+    @IBOutlet weak var flashlight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sadakoDidAppear(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0:
+            sadakoImage.image = UIImage(named: "transparent")
+        case 1:
+            sadakoImage.image = UIImage(named: "sadako")
+        default:
+            sadakoImage.image = UIImage(named: "transparent")
+        }
+        
+    }
+    
+    
+    @IBAction func sliderDidSlide(_ sender: UISlider) {
+            sadakoImage.alpha = CGFloat(
+                  slider.value / slider.maximumValue)
+    }
+    
+    @IBAction func switchDidChange(_ sender: UISwitch) {
+        if (sender.isOn == true)
+        {
+            flashlight.image = UIImage(named: "flashlight")
+        }
+        else
+        {
+            flashlight.image = UIImage(named: "transparent")
+        }
+    }
+    
+    
 }
 
